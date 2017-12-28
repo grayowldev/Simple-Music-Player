98b7773784d576ed951766c3f6250e2c6ff99f1f
grayowldev <truegrayowl@gmail.com>
2017-12-28 00:02:25 -0500

    minor updates

--- src/Coreui.java
+++ src/Coreui.java
@@ -1,26 +1,55 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javazoom.jl.player.Player;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Created by GrayOwl on 12/27/17.
  */
 public class Coreui {
+    //Parent root = FXMLLoader.load(getClass().getResource("coreui.fxml"));
+    //Scene scene = new Scene(root);
     private File mediaFile;
+    private boolean playState = false;
+
+    public Coreui() throws IOException {
+    }
+
+    @FXML
+    private Button playBtn;
 
     public void play() throws FileNotFoundException {
-        System.out.println("play clicked");
-        new Thread(()->{
-            try {
-                Player player = new Player(new FileInputStream(mediaFile));
-                player.play();
-            }catch (Exception ex){
-                ex.printStackTrace();
-            }
-        }).start();
+        System.out.println("play/pause clicked");
+
+        if (playState == true){
+            playState = false;
+            playBtn.setText("Play");
+
+        }
+        else if (playState == false){
+            playState = true;
+            playBtn.setText("Pause");
+            new Thread(()->{
+                try {
+                    Player player = new Player(new FileInputStream(mediaFile));
+                    player.play();
+
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }).start();
+
+        }
+
+
 
 
     }
--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,15 @@ import javafx.stage.Stage;
  * Created by GrayOwl on 12/27/17.
  */
 public class Main extends Application {
+     //Scene scene;
     //Stage primaryStage = new Stage();
+    Scene scene;
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("coreui.fxml"));
         primaryStage.setTitle("Simple Music player");
-        primaryStage.setScene(new Scene(root,300,400));
+        scene = new Scene(root,300,400);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
